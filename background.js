a54d142c773b5664953627e8c80edd0615e33ec0
byeolyoon <107675867+byeolyoon@users.noreply.github.com>
2023-03-06 01:48:25 +0900

    Update background.js

--- background.js
+++ background.js
@@ -4,6 +4,6 @@ const choseImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `${choseImage}`;
+bgImage.src = `.${choseImage}`;
 
 document.body.appendChild(bgImage);
